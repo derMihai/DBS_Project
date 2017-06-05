0384c6a32952b224895aa126edba832525332d23
clgp2 <bernadeta.chisarau@googlemail.com>
2017-06-05 21:59:17 +0200

    h

--- import.py
+++ import.py
@@ -8,6 +8,4 @@ cursor = mydb.cursor()
 csv_data = csv.reader(file('american-election-tweets.csv'))
 
 for row in csv_data:
-	cursor.execute ('INSERT INTO dbs(Tweet, Contains, Hashtag, ComesAlong)') 
-          'VALUES("%s", "%s", "%s")', 
-          row)
+	cursor.execute ('INSERT INTO dbs(Tweet, Contains, Hashtag, ComesAlong)' 'VALUES("%s", "%s", "%s")', row)
